6d92d1943256f7cbfa51c36d3a7aa5034f341f70
Romana <romanamutsak2005@gmail.com>
2025-05-02 14:50:45 +0300

    Додала два приклади фільмів і стилізацію

---
+++ src/data/movies.js
@@ -0,0 +1,21 @@
+// movies.js
+export const movies = [
+    {
+      id: 1,
+      title: "Барбі",
+      description: "Фантастичний світ Барбі та Кена у рожевій країні.",
+      genre: "Комедія, Фентезі",
+      poster: "https://i.imgur.com/0rVeh4x.jpg",
+      datetime: "2025-05-10 19:00"
+    },
+    {
+      id: 2,
+      title: "Крижане серце",
+      description: "Ельза вирушає в нову подорож пізнати себе.",
+      genre: "Мультфільм, Пригоди",
+      poster: "https://i.imgur.com/1hYwXzL.jpg",
+      datetime: "2025-05-11 14:30"
+    },
+    // додайте ще
+  ];
+  
